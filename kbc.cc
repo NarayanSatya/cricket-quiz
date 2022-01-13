af2139f684fc0c0b18c2b4029c2585ebaf6455b6
Satya Narayan Gope <88315566+NarayanSatya@users.noreply.github.com>
2022-01-13 11:38:29 +0530

    Add files via upload
    This is a simple sports quiz coded in a c++ language. Each question contains some points and while it progress it increases the money.  It also have an elemination round where if you loose you can't reach to finale and loose all your points. Enjoy and please give some feedback. Thank you

---
+++ kbc.cc
@@ -0,0 +1,197 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,ans,sum=0;
+    cout<<"WELCOME TO KBC"<<endl;
+    cout<<"press 1 to play"<<endl;
+    cin>>n;
+    if(n==1)
+    {
+        cout<<" 1.How many runs Ben Stokes score in 2nd ining in headingley historic test win in Ashes 2019"<<endl;cout<<endl;
+        cout<<"    option 1 135 "<<endl;
+        cout<<"    option 2 140 "<<endl;
+        cout<<"    option 3 145 "<<endl;
+        cout<<"    option 4 130 "<<endl;
+        cin>>ans;
+        if(ans==1){
+        cout<<"correct"<<endl;  cout<<" You have won 5000rs"<<endl;
+        sum=5000;cout<<endl;}
+        else{
+        cout<<"wrong  correct answer is 135"<<endl;cout<<" you loss 1000"<<endl;
+        sum=sum-1000;}
+
+        cout<<endl;
+
+
+cout<<" 2. Who won the  Ashes 2018"<<endl;cout<<endl;
+        cout<<"    option 1 Australiya "<<endl;
+        cout<<"    option 2 England "<<endl;
+        cout<<"    option 3 Drawn "<<endl;
+        cout<<"    option 4 none of the avobe "<<endl;
+        cin>>ans;
+        if(ans==2){
+       cout<<"correct"<<endl;  cout<<" You have won 10000rs"<<endl;
+        sum=10000;cout<<endl;}
+        else{
+        cout<<"wrong   correct answer is ENGLAND"<<endl;cout<<" you loss 1000"<<endl;
+        sum=sum-1000;}cout<<endl;
+
+cout<<" 3. Who won the  ICC PLAYER OF THE DECADE"<<endl;cout<<endl;
+        cout<<"    option 1 DEVID WARNER "<<endl;
+        cout<<"    option 2 VIRAT KOHLI "<<endl;
+        cout<<"    option 3 STEVE SMITH"<<endl;
+        cout<<"    option 4 AB DEVILLIARS "<<endl;
+        cin>>ans;
+        if(ans==2){
+        cout<<"correct"<<endl;  cout<<" You have won 20000rs"<<endl;
+        sum=20000;cout<<endl;}
+        else{
+        cout<<"wrong correct answer is VIRAT KOHLI"<<endl;cout<<" you loss 1000"<<endl;
+        sum=sum-1000;}cout<<endl;
+
+cout<<" 4. What is the highest individual score of MS dhoni in test"<<endl;cout<<endl;
+        cout<<"    option 1 220 "<<endl;
+        cout<<"    option 2 224 "<<endl;
+        cout<<"    option 3 230"<<endl;
+        cout<<"    option 4 241 "<<endl;
+        cin>>ans;
+        if(ans==2){
+        cout<<"correct"<<endl;  cout<<" You have won 40000rs"<<endl;
+        sum=40000;cout<<endl;}
+        else{
+        cout<<"wrong correct answer is 224"<<endl;cout<<" you loss 1000"<<endl;
+        sum=sum-1000;}cout<<endl;
+
+cout<<" 5. What is the score of SACHIN TENDULKAR in his last test"<<endl;cout<<endl;
+        cout<<"    option 1 70 "<<endl;
+        cout<<"    option 2 71 "<<endl;
+        cout<<"    option 3 74"<<endl;
+        cout<<"    option 4 73 "<<endl;
+        cin>>ans;
+        if(ans==3){
+        cout<<"correct"<<endl;  cout<<" You have won 80000rs"<<endl;
+        sum=80000;cout<<endl;}
+        else{
+        cout<<"wrong correct answer is 74"<<endl;cout<<" you loss 10000"<<endl;
+        sum=sum-10000;}cout<<endl;
+
+
+cout<<" 6. How many centuries do VIRAT KOHLI have in IPL"<<endl;cout<<endl;
+        cout<<"    option 1 4 "<<endl;
+        cout<<"    option 2 5 "<<endl;
+        cout<<"    option 3 6"<<endl;
+        cout<<"    option 4 3 "<<endl;
+        cin>>ans;
+        if(ans==2){
+        cout<<"correct"<<endl;  cout<<" You have won 160000rs"<<endl;
+        sum=160000;cout<<endl;}
+        else{
+        cout<<"wrong correct answer is 5"<<endl;cout<<" you loss 20000"<<endl;
+        sum=sum-20000;}cout<<endl;
+
+
+ cout<<" 7. Who became the Man OF The Series in ind vs aus 2014-15 Boarder-Govaskar test series"<<endl;cout<<endl;
+        cout<<"    option 1 Virat Kohli "<<endl;
+        cout<<"    option 2 Mitchell Johnson "<<endl;
+        cout<<"    option 3 Steve Smith"<<endl;
+        cout<<"    option 4 Nathan Lyon "<<endl;
+        cin>>ans;
+        if(ans==3){
+        cout<<"correct"<<endl;  cout<<" You have won 320000rs"<<endl;
+        sum=320000;cout<<endl;}
+        else{
+        cout<<"wrong correct answer is Steve Smith"<<endl;cout<<" you loss 40000"<<endl;
+        sum=sum-40000;}cout<<endl;  
+
+
+cout<<" 8. who is the first test wicket of jasprit bumrah ?"<<endl;cout<<endl;
+        cout<<"    option 1 Steve Smith "<<endl;
+        cout<<"    option 2 David Warner "<<endl;
+        cout<<"    option 3 Faf du Plessis"<<endl;
+        cout<<"    option 4 AB de Villiers "<<endl;
+        cin>>ans;
+        if(ans==4){
+        cout<<"correct"<<endl;  cout<<" You have won 640000rs"<<endl;
+        sum=640000;cout<<endl;}
+        else{
+        cout<<"wrong correct answer is AB de Villiers"<<endl;cout<<" you loss 80000"<<endl;
+        sum=sum-80000;} cout<<endl;           
+
+
+cout<<" 9. who won the ICC Mens T20I Cricketer of the Decade ?"<<endl;cout<<endl;
+        cout<<"    option 1 Chris Gayle  "<<endl;
+        cout<<"    option 2 Dawid Malan"<<endl;
+        cout<<"    option 3 Rashid Khan"<<endl;
+        cout<<"    option 4 AB de Villiers  "<<endl;
+        cin>>ans;
+        if(ans==3){
+        cout<<"correct"<<endl;  cout<<" You have won 12,50,000rs"<<endl;
+        sum=1250000;cout<<endl;}
+        else{
+        cout<<"wrong correct answer is Rashid Khan"<<endl;cout<<" you loss all money"<<endl;
+        sum=sum-160000;cout<<endl;
+        }
+
+
+cout<<"NOW ITS ELIMINATION ROUND FROM NOW-ONWORDS IF YOU LOOSE YOU WILL GET OUT OF THE GAME AND LOOSE ALL MONEY"<<endl;
+cout<<endl;
+cout<<endl;
+
+cout<<" 10. Who is the captain of u-19 indian team in 2016 ?"<<endl;cout<<endl;
+        cout<<"    option 1 Ricky Bhui  "<<endl;
+        cout<<"    option 2 Mayank Dagar"<<endl;
+        cout<<"    option 3 Yash Dull"<<endl;
+        cout<<"    option 4 Ishan Kishan "<<endl;
+        cin>>ans;
+        if(ans==4){
+        cout<<"correct"<<endl;  cout<<" You have won 25,00,000rs"<<endl;
+        sum=2500000;cout<<endl;}
+        else{
+        cout<<"wrong correct answer is Ishan Kishan"<<endl;cout<<" you loss all money"<<endl;
+        sum=0;cout<<endl;
+        }
+     if(sum!=0)
+     {
+cout<<" 11. which bowler has dismissed sachin tendulkar most times including all formats ?"<<endl;
+        cout<<"    option 1 Brett Lee (Australia)  "<<endl;
+        cout<<"    option 2 Muthiah Muralitharan (Sri Lanka)"<<endl;
+        cout<<"    option 3 Glenn McGrath (Australia)"<<endl;
+        cout<<"    option 4 Shaun Pollock (South Africa)  "<<endl;
+        cin>>ans;
+        if(ans==1){
+        cout<<"correct"<<endl;  cout<<" You have won 50,00,000rs"<<endl;
+        sum=5000000;cout<<endl;}
+        else{
+        cout<<"wrong correct answer is Brett Lee (Australia)"<<endl;cout<<" you loss all money"<<endl;
+        sum=0;cout<<endl;
+        }}
+        if(sum!=0){
+
+        cout<<"Deviyo Aur Sajjano ye rha sawal 1CRORE ka"<<endl;
+cout<<endl;
+cout<<endl;
+cout<<" 12. which kind of bowler is marnus labuschagne ?"<<endl;cout<<endl;
+        cout<<"    option 1 Right-arm off break "<<endl;
+        cout<<"    option 2 Right-arm medium "<<endl;
+        cout<<"    option 3 Right-arm leg-break"<<endl;
+        cout<<"    option 4 Right-arm off-spin "<<endl;
+        cin>>ans;
+        if(ans==3){
+        cout<<"correct"<<endl;  cout<<" You have won 1CRORE"<<endl;
+        sum=10000000;cout<<endl;}
+        else{
+        cout<<"wrong correct answer is Right-arm leg-break"<<endl;cout<<" you loss all money"<<endl;
+        sum=0;}cout<<endl;
+     
+    }
+    cout<<endl;
+    cout<<endl;
+    cout<<"total money you have recieved   =  "<<sum;
+cout<<endl;
+cout<<endl;
+    cout<<"THANK YOU FOR PLAYING HAVE A GREAT DAY";
+    }
+    return 0; 
+
+}
